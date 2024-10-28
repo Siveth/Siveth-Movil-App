63401f46bf981cffa37368a3cf139e76d2fe0eb8
AlanHH28 <alher2803@gmail.com>
2024-10-28 17:09:37 -0600

    implementación de los servicios

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
+
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -30,6 +31,10 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="Screens/Viajes" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="Sreens/Boletos" options={{ headerShown: false }} />
+        <Stack.Screen name="Sreens/Mudanza" options={{ headerShown: false }} />
+        <Stack.Screen name="Sreens/Paqueteria" options={{ headerShown: false }} /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
