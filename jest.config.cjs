2ad97997d925c960e79829d2825cb30cacfa34c9
AlanHH28 <alher2803@gmail.com>
2024-11-06 19:24:08 -0600

    cambios en el jest.config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
 module.exports = {
   preset: 'react-native',
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
   },
   testEnvironment: 'jsdom',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
 };
